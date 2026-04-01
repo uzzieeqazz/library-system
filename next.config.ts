3cb359eef71185e5add2bd9c7271b11ee00baf7f
Ataniyaz <ataniyaz@library.kz>
2026-04-01 22:19:47 +0500

    chore: remove old scripts, fix book covers timeout by serving locally

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "books.google.com",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
       },
       {
